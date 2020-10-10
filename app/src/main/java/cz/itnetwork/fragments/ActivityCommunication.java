01dafb30211e8d433495262336d88c0ba95e53d5
LuPa <lupa.lupa@seznam.cz>
2020-10-10 22:50:15 +0200

    c

--- app/src/main/java/cz/itnetwork/fragments/ActivityCommunication.java
+++ app/src/main/java/cz/itnetwork/fragments/ActivityCommunication.java
@@ -1,14 +1,70 @@
 package cz.itnetwork.fragments;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class ActivityCommunication extends AppCompatActivity {
+
+    EditText etMessage;
+    Button btnAddFragment;
+    Button btnSend;
+    TextView labelMessageFromFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_communication);
+
+        etMessage = findViewById(R.id.etMessage);
+        btnAddFragment = findViewById(R.id.btnAddFragment);
+        btnSend = findViewById(R.id.btnSend);
+        labelMessageFromFragment = findViewById(R.id.labelMessageFromFragment);
+
+        btnAddFragment.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showFragment();
+            }
+        });
+
+        btnSend.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (etMessage.getText() != null) {
+                    if (!etMessage.getText().toString().equals("")) {
+                        sendMessageToFragment(etMessage.getText().toString());
+                    }
+                }
+            }
+        });
+    }
+
+    private void showFragment() {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+
+        FragmentForCommunication fragment = new FragmentForCommunication();
+        fragmentTransaction.add(R.id.containerForCommunication, fragment, FragmentForCommunication.class.getName());
+        fragmentTransaction.commit();
+    }
+
+    private void sendMessageToFragment(String msg) {
+        FragmentManager fm = getSupportFragmentManager();
+        FragmentForCommunication fragment = (FragmentForCommunication) fm.findFragmentByTag(FragmentForCommunication.class.getName());
+
+        if (fragment != null) {
+            fragment.setMessageFromActivity(msg);
+        }
+    }
+
+    public void setMessageFromFragment(String msg) {
+        labelMessageFromFragment.setText(msg);
     }
 }
--- app/src/main/java/cz/itnetwork/fragments/FragmentForCommunication.java
+++ app/src/main/java/cz/itnetwork/fragments/FragmentForCommunication.java
@@ -5,6 +5,9 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -12,15 +15,51 @@ import androidx.fragment.app.Fragment;
 
 public class FragmentForCommunication extends Fragment {
 
+    EditText etMessage;
+    Button btnSend;
+    TextView labelMessageFromActivity;
+
+    ActivityCommunication activity;
+
     @Override
     public void onAttach(@NonNull Context context) {
         super.onAttach(context);
+
+        if (context instanceof ActivityCommunication) {
+            this.activity = (ActivityCommunication) context;
+        }
     }
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_for_communication, container, false);
+
+        etMessage = view.findViewById(R.id.etMessage);
+        btnSend = view.findViewById(R.id.btnSend);
+        labelMessageFromActivity = view.findViewById(R.id.labelMessageFromActivity);
+
+        btnSend.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (etMessage.getText() != null) {
+                    if (!etMessage.getText().toString().equals("")) {
+                        sendMessageToActivity(etMessage.getText().toString());
+                    }
+                }
+            }
+        });
+
         return view;
+    }
+
+    private void sendMessageToActivity(String msg) {
+        if (activity != null) {
+            activity.setMessageFromFragment(msg);
+        }
+    }
+
+    public void setMessageFromActivity(String msg) {
+        this.labelMessageFromActivity.setText(msg);
     }
 }
